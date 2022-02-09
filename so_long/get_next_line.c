16dbc9ff9cbe3155b26c857688b40671c3ae5273
김연주 (yeonjoo kim) <65381985+yeunjoo121@users.noreply.github.com>
2022-02-09 22:56:23 +0900

    Add files via upload

--- so_long/get_next_line.c
+++ so_long/get_next_line.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yeonjkim <yeonjkim@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/09 14:41:41 by yeonjkim          #+#    #+#             */
-/*   Updated: 2022/02/09 14:46:45 by yeonjkim         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "so_long.h"
 
-int	isin_newline(char *str)
+int		isin_newline(char *str)
 {
 	int		index;
 
@@ -28,28 +16,26 @@ int	isin_newline(char *str)
 	return (-1);
 }
 
-int	ft_error(char **backup)
+int		ft_error(char **backup)
 {
 	while (*backup != 0)
 	{
 		free(*backup);
-		*backup = 0;
+		*backup = 0;//프리하기 전 0 넣으면 메모리 주소가 사라지게 되므로 free할 수 없음.
 	}
 	return (-1);
 }
 
-int	get_one_line(char **backup, char **line, int cut)
+int					get_one_line(char **backup, char **line, int cut)
 {
-	char	*temp;
+	char			*temp;
 
 	(*backup)[cut] = '\0';
-	*line = ft_strdup(*backup);
-	if (!(*line))
+	if (!(*line = ft_strdup(*backup)))
 	{
 		return (ft_error(backup));
 	}
-	temp = ft_strdup(*backup + cut + 1);
-	if (!temp)
+	if (!(temp = ft_strdup(*backup + cut + 1)))
 	{
 		return (ft_error(backup));
 	}
@@ -58,24 +44,25 @@ int	get_one_line(char **backup, char **line, int cut)
 	return (1);
 }
 
-int	get_last(char **backup, char **line)
+int					get_last(char **backup, char **line)
 {
-	int	cut;
+	int				cut;
 
-	if (!(*backup))
+	if (!(*backup))//file이 비어있을 때. backup[fd]가 아무것도 할당이 되지 않았으므로 여기에 들어간다.(ft_strjoin을 한번도 실행하지 않았을 때)
 	{
 		*line = ft_strdup("");
 		return (0);
 	}
 	else
 	{
-		cut = isin_newline(*backup);
-		if (cut >= 0)
+		if ((cut = isin_newline(*backup)) >= 0)
+		//*backup == backup[fd]. *backup이 str의 제일 첫번째 주소값이므로 backup이 할당되어 있으면 아래로 들어감.
+		//할당이 안되어있으면 위로 들어감.
 		{
 			return (get_one_line(backup, line, cut));
 		}
-		*line = ft_strdup(*backup);
-		if (!*line)
+		//newline이 없으면
+		if (!(*line = ft_strdup(*backup)))
 		{
 			return (ft_error(backup));
 		}
@@ -85,7 +72,7 @@ int	get_last(char **backup, char **line)
 	}
 }
 
-int	get_next_line(int fd, char **line)
+int					get_next_line(int fd, char **line)
 {
 	static char		*backup[OPEN_MAX];
 	char			buf[BUFFER_SIZE + 1];
@@ -94,19 +81,16 @@ int	get_next_line(int fd, char **line)
 
 	if ((fd < 0) || (line == 0) || (BUFFER_SIZE <= 0))
 		return (-1);
-	readsize = read(fd, buf, BUFFER_SIZE);
-	while (readsize > 0)
+	while ((readsize = read(fd, buf, BUFFER_SIZE)) > 0)//읽어들인 크기
 	{
 		buf[readsize] = '\0';
 		backup[fd] = ft_strjoin_gnl(backup[fd], buf);
-		cut = isin_newline(backup[fd]);
-		if (cut >= 0)
+		if ((cut = isin_newline(backup[fd])) >= 0)
 			return (get_one_line(&backup[fd], line, cut));
-		readsize = read(fd, buf, BUFFER_SIZE);
 	}
 	if (readsize < 0)
 	{
 		return (ft_error(&backup[fd]));
 	}
-	return (get_last(&backup[fd], line));
+	return (get_last(&backup[fd], line));//읽어들인 게 0일 경우, backup에 남아있는 라인을 line에 저장.
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yeonjkim <yeonjkim@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/09 22:15:59 by yeonjkim          #+#    #+#             */
-/*   Updated: 2022/02/09 22:16:41 by yeonjkim         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -26,8 +14,10 @@
 
 #define X_EVENT_KEY_PRESS		2
 #define X_EVENT_KEY_release		3
-#define X_EVENT_KEY_EXIT		17
+#define X_EVENT_KEY_EXIT		17 //exit key code
 
+//Mac key code example
+//All the key code example other than below is described on the site linked in READEME.md
 #define KEY_ESC			53
 # define KEY_Q			12
 # define KEY_W			13
@@ -47,7 +37,7 @@ struct draw_arg
     void *mlx;
     void *win;
     void *wall[5];
-    void *collect[5];
+    void *collection[5];
     void *player[4];
     void *exit;
     void *black;
@@ -86,7 +76,7 @@ char	**ft_split(char const *s, char c);
 char    *ft_strjoin(char const *s1, char const *s2);
 int is_valid_char(char c);
 void is_surrounded_by_wall(char **map, int width, int length);
-void map_arg_init(int i, int j, char **map, struct map_arg *m);
+void map_arg_init(int i, int j, char **map, struct map_arg *m, int *playerflag);
 void is_rectangle(char **map, struct map_arg *m);
 void is_all_valid_char(char **map, int width, int length, struct map_arg *m);
 void so_long_error(int errorcode);
@@ -96,20 +86,7 @@ void map_parsing(int fd, char ***map);
 void    ft_putchar_fd(char c, int fd);
 void    ft_putnbr_fd(int n, int fd);
 
-void key_arg_init(struct key_arg *key, struct map_arg *m);
-int xkey_clicked();
-void draw_init(struct draw_arg *d, struct map_arg *m, int *w, int *h);
+//gui.c
 void show_window(struct map_arg *m, char **map);
-
-void print_tile_move(struct key_arg *key);
-void	draw_current(struct key_arg *key, int act, int i, int j);
-void	draw_prev(struct key_arg *key, int i, int j);
-void print_tile(struct map_arg *m, char **map, struct draw_arg draw, int index);
-
-void wkey_press(struct key_arg *key);
-void skey_press(struct key_arg *key);
-void akey_press(struct key_arg *key);
-void dkey_press(struct key_arg *key);
-int key_press(int keycode, struct key_arg *key);
 
 #endif
